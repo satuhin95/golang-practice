b9a5a1a820bf680aa96bb5d3bed5918973a4af26
saifcm95 <saifcm95@gmail.com>
2023-10-18 11:42:46 +0600

    go fiber caching

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"go-cache-api/middleware"
+	"go-cache-api/routes"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/patrickmn/go-cache"
+)
+
+func main() {
+	app := fiber.New() // Creating a new instance of Fiber.
+
+	cache := cache.New(10*time.Minute, 20*time.Minute) // setting default expiration time and clearance time.
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
+	app.Get("/posts/:id", middleware.CacheMiddleware(cache), routes.GetPosts) //commenting this route just to test the "/" endpoint.
+	app.Listen(":8080")
+}
